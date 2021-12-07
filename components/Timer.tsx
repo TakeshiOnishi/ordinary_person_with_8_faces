623ac6ed5bc938dd24a85545ddd9deaa1e0feee7
suzuki.masato <suzuki.masato@a-tm.co.jp>
2021-12-08 05:14:26 +0900

    調整

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@mui/material";
+import { styled } from "@mui/system";
 import React, { useState, useRef, useImperativeHandle, forwardRef, useEffect, useCallback } from "react";
 import useStopwatch from '../hooks/useStopwatch';
 import { zeroPadding } from "../uitl/decorator";
+
+
+const H1 = styled('h1')({
+  marginBottom: '0'
+})
 
 const Stopwatch: React.VFC = () => {
 
@@ -16,11 +22,11 @@ const Stopwatch: React.VFC = () => {
 
   return (
     <div style={{textAlign: 'center'}}>
-      <h1>経過時間</h1>
-      <div style={{fontSize: '100px'}}>
+      <H1>経過時間</H1>
+      <div style={{fontSize: '90px', lineHeight: '1em'}}>
         <span>{zeroPadding(minutes)}</span>:<span>{zeroPadding(seconds)}</span>:<span>{zeroPadding(seconds100)}</span>
       </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <p style={{marginBottom: '0', marginTop: '0'}}>{isRunning ? 'Running' : 'Not running'}</p>
       <button onClick={start}>Start</button>
       <button onClick={pause}>Pause</button>
       {/* <button onClick={reset}>Reset</button> */}
