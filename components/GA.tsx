58e7cb4c5c893d40650d0e1edb80a7c866b08806
Onishi Takeshi <march.hare.come@gmail.com>
2021-12-13 20:53:58 +0900

    GAリンクのみ追加

---
+++ components/GA.tsx
@@ -0,0 +1,21 @@
+import Script from 'next/script'
+
+const GA = () => {
+  return (
+    <>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'UA-215137358-1', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <Script
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="afterInteractive"
+      />
+    </>
+  )
+}
+
+export default GA
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import React from "react";
 import dynamic from 'next/dynamic'
 const Board = dynamic(import('../components/Board'), { ssr: false });
+import GA from '../components/GA';
 
 const Other:React.VFC = () => {
 
@@ -10,6 +11,7 @@ const Other:React.VFC = () => {
       <Head>
         <title>普通人8面相ゲーム</title>
       </Head>
+      <GA />
       <Board />
     </>
   )
